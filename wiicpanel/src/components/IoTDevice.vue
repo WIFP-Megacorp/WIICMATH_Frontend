5013b87744a25bee45fab2b269f32da668581a63
Eirik B <87980291+Eiriksb@users.noreply.github.com>
2023-09-24 12:58:58 +0200

    Initial Push

---
+++ wiicpanel/src/components/IoTDevice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="iot-device">
+    <h3>{{ deviceName }}</h3>
+    <p>Current Temperature: {{ currentTemperature }}°C</p>
+    <p>Humidity: {{ currentHumidity }}%</p>
+    <button class="iot-button" @click="selectDevice">Select</button>
+  </div>
+</template>
+  
+<script>
+export default {
+  props: {
+    deviceName: String,
+    currentTemperature: Number, // Add this prop
+    currentHumidity: Number, // Add this prop
+  },
+  methods: {
+    selectDevice() {
+      this.$emit("select-device", this.deviceName);
+    },
+  },
+};
+</script>
