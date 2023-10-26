7fb1d9b9ea6478753c5688eb17a9d1498424f343
Trivinyx <tom.a.s.myre@gmail.com>
2023-10-26 04:20:03 +0200

    Fixed bug with deployed version not accepting hostname.

--- wiicpanel/vue.config.js
+++ wiicpanel/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        allowedHosts: [
+            'localhost',
+            'vps.megacorp.tech',
+            '162.248.102.181',
+        ],
+    },
 })
